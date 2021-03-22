338909971578e04163bebf65a54f38001bba024b
DwGoing <jianghy1209@163.com>
2021-03-22 15:56:40 +0800

    🧪test:

--- src/migrations/2_deploy_contracts.js
+++ src/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const ElephantByAntToekn = artifacts.require("ElephantByAntToekn");
+const AToken = artifacts.require("AToken");
 
 module.exports = function (deployer) {
     deployer.deploy(ElephantByAntToekn);
+    deployer.deploy(AToken);
 };
